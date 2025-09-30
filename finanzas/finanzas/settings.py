91ae45d2c8205f50e43754b0ad265d1616d4b276
1MT3DDY <thebenja1222@gmail.com>
2025-09-30 16:51:35 -0300

    Añadido el proyecto hasta ahora

--- finanzas/finanzas/settings.py
+++ finanzas/finanzas/settings.py
@@ -12,17 +12,15 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_DIRS = [os.path.join(BASE_DIR,'static')]
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4nau$a*db%5imzh$pytwx*1d*$rdz_ck_phfvw!igou2sid5we'
+SECRET_KEY = 'django-insecure-=_to#rz9wh!!seyk84th^ce3d&glyk52hkn8=9$^xfpea4de#_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
 ]
 
 MIDDLEWARE = [
