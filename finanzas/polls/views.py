697c9522d4d0b4d02e90bff2bf7fa06a6ffadff8
1MT3DDY <124065262+1MT3DDY@users.noreply.github.com>
2025-09-30 20:25:28 -0300

    Delete finanzas directory

--- finanzas/polls/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    return HttpResponse("Hola mumdo. estoy en el index de polls")
